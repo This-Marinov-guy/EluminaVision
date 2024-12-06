6506b4eb57a49981e9b4b8fe9deeb0aba61193b1
Ivan <ivancatan780@gmail.com>
2024-12-06 16:41:50 +0200

    add: nav hover animation

--- src/components/_module/nav/index.tsx
+++ src/components/_module/nav/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Image from "next/image";
 import classNames from "classnames";
 import { Link as ScrollLink } from "react-scroll";
+import { motion } from "framer-motion";
 
 import styles from "./style.module.scss";
 
@@ -21,8 +22,27 @@ const NavBar = () => {
       </ScrollLink>
       <div className={styles.navs}>
         {linkMap.map((item) => (
-          <ScrollLink to={item.link} smooth={true} duration={500} key={item.name} href={item.link}>
-            <div className={classNames(styles.nav, { [styles.active]: item.name === "About" })}>{item.name}</div>
+          <ScrollLink
+            to={item.link}
+            smooth={true}
+            duration={500}
+            key={item.name}
+            href={item.link}
+            activeClass="text-opacity-50"
+            className={classNames(styles.nav)}
+          >
+            <motion.div
+              initial={{ y: 0, opacity: 1 }}
+              whileHover={{
+                y: [-20, 0],
+                opacity: [0, 1],
+                transition: {
+                  duration: 0.5,
+                },
+              }}
+            >
+              <div className="h-10">{item.name}</div>
+            </motion.div>
           </ScrollLink>
         ))}
       </div>
