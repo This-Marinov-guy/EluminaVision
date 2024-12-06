6753f9e52ef1d6270367ad251d41797758df51f6
Ivan <ivancatan780@gmail.com>
2024-12-06 14:23:10 +0200

    fix: footer responsive issue

--- src/components/_module/hero/index.tsx
+++ src/components/_module/hero/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import Slider from "react-slick";
 
 import Button from "../../_basic/button";
 
 import styles from "./style.module.scss";
 
 const HeroSection = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <section className={styles.wrapper}>
       <div className={styles.heroContentWrapper}>
@@ -13,6 +22,7 @@ const HeroSection = () => {
           <h1 className={styles.heading}>Elumina Vision</h1>
           <Button className={styles.button}>SEE MORE</Button>
         </div>
+        <Slider {...settings}></Slider>
       </div>
       <div className={styles.carouselWrapper}></div>
     </section>
