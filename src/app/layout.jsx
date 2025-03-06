b390ac5a6177f28ebb507d69c43c6f28644567a3
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2025-03-06 23:25:37 +0200

    add clarity

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { Poppins } from "next/font/google";
 import { StoreProvider } from "../stores/storeProvider";
 import Footer from "../components/_module/footer";
+import Script from "next/script";
 
 import "./globals.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -50,6 +51,18 @@ export default function RootLayout({ children }) {
           </ChakraProvider>
         </StoreProvider>
       </body>
+      <Script id="clarity-script" strategy="afterInteractive">
+        {`
+        (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          t.onload = function() {
+            window.clarity && window.clarity("consent");
+          };
+        })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
+      `}
+      </Script>
     </html>
   );
 }
--- src/app/redirect/page.tsx
+++ src/app/redirect/page.tsx
@@ -1,9 +1,52 @@
 import React from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
+import styles from "./style.module.scss";
 export const dynamicParams = true;
 
 const Redirect = () => {
-  return <div>Bai hui</div>;
+  return (
+    <section id="loading" className={styles.wrapper}>
+      <motion.div
+        initial={{ x: 0 }}
+        animate={{ x: "-100%", display: "none" }}
+        transition={{
+          delay: 0.8,
+          duration: 0.3,
+        }}
+        style={{ position: "absolute", left: 0, width: "50%", height: "100vh", backgroundColor: "#010101" }}
+      ></motion.div>
+      <motion.div
+        initial={{ x: 0 }}
+        animate={{ x: "100%", display: "none" }}
+        transition={{
+          delay: 0.8,
+          duration: 0.3,
+        }}
+        style={{
+          position: "absolute",
+          right: 0,
+          width: "calc(50% + 2px)",
+          height: "100vh",
+          backgroundColor: "#010101",
+        }}
+      ></motion.div>
+      <motion.div
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 0, display: "none" }}
+        transition={{
+          delay: 0.5,
+          duration: 0.3,
+        }}
+      >
+        <div className={styles.loaderSection}>
+          <Image src="/img/logo.png" alt={"loading"} width={100} height={50} className={styles.logo} />
+          <p className={styles.text}>Loading...</p>
+        </div>
+      </motion.div>
+    </section>
+  );
 };
 
 export default Redirect;
