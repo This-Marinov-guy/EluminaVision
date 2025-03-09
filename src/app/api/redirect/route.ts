faa4ca2e3269d56b011666d7dbdbdf371d0e45e7
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2025-03-09 12:12:02 +0200

    add check

--- src/app/api/redirect/route.ts
+++ src/app/api/redirect/route.ts
@@ -8,11 +8,19 @@ export async function GET(req) {
   if (!id) {
     return NextResponse.json({ error: "Missing id parameter" }, { status: 400 });
   }
-  
+
   const { data, error } = await supabase.from("qr_codes").select("link").eq("id", id).maybeSingle();
-  
+
   if (error || !data?.link) {
     return NextResponse.json({ error: "QR Code not found" }, { status: 404 });
+  }
+
+  if (!data.link.startsWith("https://")) {
+    data["link"] = "https://" + data.link;
+  }
+
+  if (!data.link.startsWith("https://")) {
+    return NextResponse.json({ error: "Invalid URL - Please check it again: " + data.link }, { status: 500 });
   }
 
   return NextResponse.redirect(data.link, 301);
