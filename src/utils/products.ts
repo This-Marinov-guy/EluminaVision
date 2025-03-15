d56a5b1b5686b4a573c213819b511caa0b129e9c
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2025-03-15 20:30:02 +0200

    change price

--- src/utils/products.ts
+++ src/utils/products.ts
@@ -48,6 +48,6 @@ export const QR_CODES_VARIANTS = [
     limit: 5,
     currency: "GBP",
     imageUrl: "/img/products/qrCodes1.png",
-    priceId: "price_1QvZqWLUXmDaRfFYUDeWIwzv",
+    priceId: "price_1R2zSmLUXmDaRfFYmYARxbeW",
   },
 ];
