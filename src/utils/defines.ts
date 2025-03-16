3f4fef8cfe490fa1a555794df2befb098203f3b1
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2025-03-16 11:11:21 +0200

    change email

--- src/utils/defines.ts
+++ src/utils/defines.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-export const EMAIL = "Eluminavision@gmail.com";
+export const EMAIL = "info@eluminavision.com";
 export const FACEBOOK = "https://www.facebook.com/profile.php?id=61557462632493";
 export const INSTAGRAM = "https://www.instagram.com/eluminavision";
 export const PHONE = "+44 7572 798567";
